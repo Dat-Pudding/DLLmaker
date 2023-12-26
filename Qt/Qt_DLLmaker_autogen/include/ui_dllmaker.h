574d99447bd8cb4ecda309fbe7e65f9eb1d2a270
Dat-Pudding <shuto.pudding@gmail.com>
2023-12-26 19:07:09 +0100

    Added "Saving" functionality
    - rearranged UI elements => new "Options" section

--- Qt/Qt_DLLmaker_autogen/include/ui_dllmaker.h
+++ Qt/Qt_DLLmaker_autogen/include/ui_dllmaker.h
@@ -11,6 +11,7 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
 #include <QtWidgets/QGroupBox>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
@@ -30,42 +31,54 @@ public:
     QGroupBox *groupBox;
     QPushButton *exeSelectButton;
     QLineEdit *exeFilePathInput;
+    QGroupBox *groupBox_3;
+    QCheckBox *shortcutCheckBox;
+    QCheckBox *saveCheckBox;
 
     void setupUi(QMainWindow *DLLmaker)
     {
         if (DLLmaker->objectName().isEmpty())
             DLLmaker->setObjectName("DLLmaker");
-        DLLmaker->resize(360, 190);
-        DLLmaker->setMinimumSize(QSize(360, 190));
-        DLLmaker->setMaximumSize(QSize(360, 190));
+        DLLmaker->resize(420, 190);
+        DLLmaker->setMinimumSize(QSize(370, 190));
+        DLLmaker->setMaximumSize(QSize(420, 190));
         centralwidget = new QWidget(DLLmaker);
         centralwidget->setObjectName("centralwidget");
         groupBox_2 = new QGroupBox(centralwidget);
         groupBox_2->setObjectName("groupBox_2");
-        groupBox_2->setGeometry(QRect(10, 100, 341, 81));
+        groupBox_2->setGeometry(QRect(10, 100, 401, 81));
         runButton = new QPushButton(groupBox_2);
         runButton->setObjectName("runButton");
-        runButton->setGeometry(QRect(230, 50, 101, 24));
+        runButton->setGeometry(QRect(290, 50, 101, 24));
         QFont font;
         font.setPointSize(10);
         font.setBold(true);
         runButton->setFont(font);
         generatorInput = new QLineEdit(groupBox_2);
         generatorInput->setObjectName("generatorInput");
-        generatorInput->setGeometry(QRect(10, 20, 321, 24));
+        generatorInput->setGeometry(QRect(10, 20, 381, 24));
         generatorExeSelectButton = new QPushButton(groupBox_2);
         generatorExeSelectButton->setObjectName("generatorExeSelectButton");
         generatorExeSelectButton->setGeometry(QRect(10, 50, 141, 24));
         groupBox = new QGroupBox(centralwidget);
         groupBox->setObjectName("groupBox");
-        groupBox->setGeometry(QRect(10, 10, 341, 81));
+        groupBox->setGeometry(QRect(10, 10, 271, 81));
         exeSelectButton = new QPushButton(groupBox);
         exeSelectButton->setObjectName("exeSelectButton");
         exeSelectButton->setGeometry(QRect(10, 50, 141, 24));
         exeFilePathInput = new QLineEdit(groupBox);
         exeFilePathInput->setObjectName("exeFilePathInput");
-        exeFilePathInput->setGeometry(QRect(10, 20, 321, 24));
+        exeFilePathInput->setGeometry(QRect(10, 20, 251, 24));
         exeFilePathInput->setDragEnabled(true);
+        groupBox_3 = new QGroupBox(centralwidget);
+        groupBox_3->setObjectName("groupBox_3");
+        groupBox_3->setGeometry(QRect(290, 10, 121, 81));
+        shortcutCheckBox = new QCheckBox(groupBox_3);
+        shortcutCheckBox->setObjectName("shortcutCheckBox");
+        shortcutCheckBox->setGeometry(QRect(10, 20, 111, 22));
+        saveCheckBox = new QCheckBox(groupBox_3);
+        saveCheckBox->setObjectName("saveCheckBox");
+        saveCheckBox->setGeometry(QRect(10, 50, 101, 22));
         DLLmaker->setCentralWidget(centralwidget);
 
         retranslateUi(DLLmaker);
@@ -79,8 +92,11 @@ public:
         groupBox_2->setTitle(QCoreApplication::translate("DLLmaker", "Choose and run desired windeployqt.exe", nullptr));
         runButton->setText(QCoreApplication::translate("DLLmaker", "RUN", nullptr));
         generatorExeSelectButton->setText(QCoreApplication::translate("DLLmaker", "Choose WinDeployQt...", nullptr));
-        groupBox->setTitle(QCoreApplication::translate("DLLmaker", "Choose location of desired Executable", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("DLLmaker", "Choose location of your project's executable", nullptr));
         exeSelectButton->setText(QCoreApplication::translate("DLLmaker", "Choose executable...", nullptr));
+        groupBox_3->setTitle(QCoreApplication::translate("DLLmaker", "Options", nullptr));
+        shortcutCheckBox->setText(QCoreApplication::translate("DLLmaker", "Create Shortcut", nullptr));
+        saveCheckBox->setText(QCoreApplication::translate("DLLmaker", "Save Paths", nullptr));
     } // retranslateUi
 
 };
